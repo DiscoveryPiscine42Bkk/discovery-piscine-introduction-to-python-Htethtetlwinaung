cc038591a5a5628d0652aac0ef557817ea6c9041
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-23 23:30:06 +0700

    ex00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+number = input("Enter a number : ")
+if int(number) != 0:
+    print("This number is different from zero.")
+else:
+    print("This number is equal to zero.")
+#chmod +x iszero.py
