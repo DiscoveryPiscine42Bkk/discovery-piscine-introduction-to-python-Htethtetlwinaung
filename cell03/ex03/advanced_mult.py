a9a6ecbaa349731889be6e9c0702ec02284bc895
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-24 00:50:25 +0700

    ex03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+for i in range(0, 11):  
+        print(f"\nTable de {i} : ",end="")
+        for j in range(0, 11):
+            print(i * j, end=" ")
+#chmod +x advanced_mult.py
