61d51c50addb57cc2be7e7ddcb7db707f2610c79
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-23 23:45:21 +0700

    ex02

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+password = "Python is awesome"
+P = input("Enter a password : ")
+if P == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
+#chmod +x password.py
