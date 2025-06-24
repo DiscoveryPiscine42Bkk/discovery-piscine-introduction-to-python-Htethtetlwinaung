1b2d3a5cabab595c959a5b07c231e55c4a65e068
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-24 22:35:33 +0700

    ex04

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 def main():
-    count = len(sys.argv) - 1  # Exclude script name
+    count = len(sys.argv) - 1
     print(f"Number of parameters: {count}.")
 if __name__ == "__main__":
     main()
