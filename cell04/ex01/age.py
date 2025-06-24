3fdb7e3a07879bafd48e069e130cd6229abb989d
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-24 11:39:55 +0700

    ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+age = int(input("Please tell me your age: "))
+print(f"You are {age} years old.")
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
+#chmod +x upcase_it.py
