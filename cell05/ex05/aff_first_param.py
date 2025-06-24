8e888da22f63ce9fe38dabd534918c24dd51fa32
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-24 15:32:49 +0700

    ex05

---
+++ cell05/ex05/aff_first_param.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+def main():
+    if len(sys.argv) > 1: 
+        print(sys.argv[1])
+    else:
+        print("none")
+if __name__ == "__main__":
+    main()
+#chmod +x aff_forst_param.py
