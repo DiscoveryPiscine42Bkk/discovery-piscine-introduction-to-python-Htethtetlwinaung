d12cfd86aef99b667fb532b9420d3a6a371cd179
Htethtetlwinaung <66011000@kmitl.ac.th>
2025-06-24 13:03:28 +0700

    ex02

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+original_array = [2,8,9,48,8,22,-12,2]
+new_array = [x+2 for x in original_array if x > 5]
+print("Original array: ", original_array)
+print("New array: " ,new_array)
+#chmod +x play_with_arrays.py
